3c371ca859c4831856cec12655e6e10976f40439
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-02-25 10:46:01 +0600

    commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,32 @@
-import React, {useContext} from 'react';
-import logo from '../../images/logo.png';
-import './Header.css';
-import {Link} from "react-router-dom";
-import {LoggedUserContext} from "../../App";
+import React, { useContext } from "react";
+import logo from "../../images/logo.png";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { LoggedUserContext } from "../../App";
 
 const Header = (props) => {
-  const [loggedUser, setLoggedUser] = useContext(LoggedUserContext); 
+  const [loggedUser, setLoggedUser] = useContext(LoggedUserContext);
   return (
-  <header className="header">
-  <Link to="/"><img src={logo} alt="ema-john" /></Link>  
-  <nav>
-    <Link to="/shop">Shop</Link>
-    <Link to="/review">Order Review</Link>
-    <Link to="/inventory">Manage Inventory</Link>
-    { loggedUser.email && 
-    <button onClick={() => setLoggedUser({})}>Logout</button>
-    }
-  </nav>
-  </header> 
-  )
-}
- 
+    <header className="header">
+      <Link to="/">
+        <img src={logo} alt="ema-john" />
+      </Link>
+      <nav>
+        <Link to="/shop">Shop</Link>
+        <Link to="/review">Order Review</Link>
+        <Link to="/inventory">Manage Inventory</Link>
+        {loggedUser.email ? (
+          <button onClick={() => setLoggedUser({})}>
+            Logout: {loggedUser?.username}
+          </button>
+        ) : (
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        )}
+      </nav>
+    </header>
+  );
+};
+
 export default Header;
